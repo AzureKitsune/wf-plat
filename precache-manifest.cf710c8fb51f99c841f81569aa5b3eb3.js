9ed544f2628a1a9009d6a629ee74bf628391055e
Amrykid <amrykid@gmail.com>
2019-09-09 18:00:28 -0500

    Updates

---
+++ precache-manifest.cf710c8fb51f99c841f81569aa5b3eb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d1735bfa9a10484e65709e67dcb55e",
+    "url": "/wf-plat/index.html"
+  },
+  {
+    "revision": "14b0c2227512648cbe8e",
+    "url": "/wf-plat/static/css/main.30c713a0.chunk.css"
+  },
+  {
+    "revision": "ea42b2d6bf662ef993e4",
+    "url": "/wf-plat/static/js/2.13e2cdc7.chunk.js"
+  },
+  {
+    "revision": "14b0c2227512648cbe8e",
+    "url": "/wf-plat/static/js/main.6bb89a48.chunk.js"
+  },
+  {
+    "revision": "df75497c749eb4d2857e",
+    "url": "/wf-plat/static/js/runtime~main.a4b5d517.js"
+  }
+]);
